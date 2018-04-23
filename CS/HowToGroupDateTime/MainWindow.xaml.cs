6f7a79db80dee675d66444307de4039efa9f2944
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/HowToGroupDateTime/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Data.OleDb;
+using System.Windows;
+using DevExpress.Xpf.PivotGrid;
+using HowToBindToMDB.NwindDataSetTableAdapters;
+
+namespace HowToBindToMDB {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        NwindDataSet.SalesPersonDataTable salesPersonDataTable = new NwindDataSet.SalesPersonDataTable();
+        SalesPersonTableAdapter salesPersonDataAdapter = new SalesPersonTableAdapter();
+
+        public MainWindow() {
+            InitializeComponent();
+            pivotGridControl1.DataSource = salesPersonDataTable;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) {
+            salesPersonDataAdapter.Fill(salesPersonDataTable);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Group Date-Time Values
+
+
+<p>This example shows how to group date-time values.</p>
+
+<br/>
+
+
