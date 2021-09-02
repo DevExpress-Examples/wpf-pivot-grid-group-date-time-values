c645953db84f25eafae514e5ffb146d6ab5e1b70
DevExpressExampleBot <fakeEmail>
2021-09-02 20:02:34 +0300

    autogenerate VB

--- VB/HowToGroupDateTime/MainWindow.xaml.vb
+++ VB/HowToGroupDateTime/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
+﻿Imports System.Data
 Imports System.Data.OleDb
 Imports System.Windows
 Imports DevExpress.Xpf.PivotGrid
@@ -11,16 +10,18 @@ Namespace HowToBindToMDB
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
+
+'INSTANT VB NOTE: The variable salesPersonDataTable was renamed since it may cause conflicts with calls to static members of the user-defined type with this name:
+		Private salesPersonDataTable_Conflict As New NwindDataSet.SalesPersonDataTable()
 		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
 		Public Sub New()
 			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
+			pivotGridControl1.DataSource = salesPersonDataTable_Conflict
 		End Sub
 
 		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
+			salesPersonDataAdapter.Fill(salesPersonDataTable_Conflict)
 		End Sub
 	End Class
 End Namespace
