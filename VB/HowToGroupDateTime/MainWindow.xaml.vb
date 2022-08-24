6ed28d45fc648b357d0fbf765da4d103fbede0df
DevExpressExampleBot <fakeEmail>
2022-08-24 18:39:40 +0400

    Source auto update [skip ci]

--- VB/HowToGroupDateTime/MainWindow.xaml.vb
+++ VB/HowToGroupDateTime/MainWindow.xaml.vb
@@ -1,26 +1,25 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Data
-Imports System.Data.OleDb
 Imports System.Windows
-Imports DevExpress.Xpf.PivotGrid
 Imports HowToBindToMDB.NwindDataSetTableAdapters
 
 Namespace HowToBindToMDB
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
-		Private salesPersonDataTable As New NwindDataSet.SalesPersonDataTable()
-		Private salesPersonDataAdapter As New SalesPersonTableAdapter()
 
-		Public Sub New()
-			InitializeComponent()
-			pivotGridControl1.DataSource = salesPersonDataTable
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
-			salesPersonDataAdapter.Fill(salesPersonDataTable)
-		End Sub
-	End Class
+        Private salesPersonDataTable As NwindDataSet.SalesPersonDataTable = New NwindDataSet.SalesPersonDataTable()
+
+        Private salesPersonDataAdapter As SalesPersonTableAdapter = New SalesPersonTableAdapter()
+
+        Public Sub New()
+            Me.InitializeComponent()
+            Me.pivotGridControl1.DataSource = salesPersonDataTable
+        End Sub
+
+        Private Sub Window_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            salesPersonDataAdapter.Fill(salesPersonDataTable)
+        End Sub
+    End Class
 End Namespace
